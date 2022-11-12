5543414f6dad9157a03ab423098f7d753d8dc12a
Jaturaphum Suaprakhon <106952984+Jaturaphum@users.noreply.github.com>
2022-11-12 11:03:11 +0700

    Add files via upload

---
+++ ILOVEYOU.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+int main() 
+{
+    clescr();
+    int i;
+    textcolor(red);
+    textbackground(white);
+    for(i=1; i<120; i++)
+    {
+        peintf("I LOVE YOU \3");
+        daley(200);
+        {
+            getch();
+        }
+    }
+}

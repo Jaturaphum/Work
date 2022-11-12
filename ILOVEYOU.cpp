ed857b18e3aeb642632a655f8abee5373894f99c
Jaturaphum Suaprakhon <106952984+Jaturaphum@users.noreply.github.com>
2022-11-12 12:11:29 +0700

    Update ILOVEYOU.cpp

--- ILOVEYOU.cpp
+++ ILOVEYOU.cpp
@@ -1,18 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 #include<dos.h>
-int main() 
-{
-    clescr();
+void main() {
+    clrscr();
     int i;
     textcolor(RED);
     textbackground(WHITE);
-    for(i=1; i<120; i++)
-    {
+    for(i=1; i<120; i++) {
         cprintf("I LOVE YOU \3");
         daley(200);
         {
             getch();
         }
     }
-}
